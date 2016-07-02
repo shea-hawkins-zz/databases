2c4964fd6ae3e7aa45d5c06023a25573e9a265ff
Hack Reactor Students <accounts@hackreactor.com>
2016-07-01 20:23:50 -0700

    Setup schema and connect to database

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,17 @@
-var mysql = require('mysql');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : 'root'
+});
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
 
+  console.log('connected as id ' + connection.threadId);
+});
 
+module.exports.connection = connection;
--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,81 @@
+drop database chat;
 CREATE DATABASE chat;
-
 USE chat;
-
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
-/* Create other tables and define schemas for them here! */
-
-
-
-
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+
+ -- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+    
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `user_id` INTEGER NOT NULL,
+  `room_id` INTEGER NOT NULL,
+  `message` VARCHAR(255) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+    
+CREATE TABLE `users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'rooms'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `rooms`;
+    
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room_id) REFERENCES `rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`user_id`,`room_id`,`message`) VALUES
+-- ('','','','');
+-- INSERT INTO `users` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `rooms` (`id`,`name`) VALUES
+-- ('','');
 
