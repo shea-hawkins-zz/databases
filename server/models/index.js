027a84bf8f3052499a9cd54176e6788af6e07749
Hack Reactor Students <accounts@hackreactor.com>
2016-07-02 11:20:20 -0700

    Add user handling to modelw

--- server/models/index.js
+++ server/models/index.js
@@ -2,39 +2,61 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+      return new Promise(function(resolve, reject) {
+        var queryStr = `
+          SELECT message, users.name username, rooms.name roomname
+          FROM messages 
+            INNER JOIN users on messages.user_id = users.id
+            INNER JOIN rooms on messages.room_id = rooms.id
+        `;
+        db.query(queryStr, function(err, results) {
+          !err ? resolve(results) : reject(err);
+        });
+      });
+    }, // a function which produces all the messages
     post: function (msgObj) { // a function which can be used to insert a message into the database
-      console.log('msg', msgObj);
-      var queryStr = `
-        INSERT IGNORE INTO rooms (name)
-        SELECT '${msgObj.roomname}';
-        INSERT IGNORE INTO users (name)
-        SELECT '${msgObj.username}';
-        INSERT IGNORE INTO messages (message, user_id, room_id)
-        SELECT '${msgObj.message}', users.id, rooms.id
-        FROM users, rooms
-        WHERE users.name like '${msgObj.username}'
-          AND rooms.name like '${msgObj.roomname}';
-      `;
-      db.query(queryStr, function(err) {
-        if (err) {
-          throw err;
-        } else {
-          console.log('message inserted: ', msgObj.message);
-        }
+      return new Promise(function(resolve, reject) {
+        var queryStr = `
+          INSERT IGNORE INTO rooms (name)
+          SELECT '${msgObj.roomname}';
+          INSERT IGNORE INTO users (name)
+          SELECT '${msgObj.username}';
+          INSERT IGNORE INTO messages (message, user_id, room_id)
+          SELECT '${msgObj.message}', users.id, rooms.id
+          FROM users, rooms
+          WHERE users.name like '${msgObj.username}'
+            AND rooms.name like '${msgObj.roomname}';
+        `;
+        db.query(queryStr, function(err) {
+          !err ? resolve() : reject(err);
+        });
       });
     } 
   },
-
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      return new Promise(function(resolve, reject) {
+        var queryStr = `
+          SELECT users.name username 
+          FROM users;
+        `;
+        db.query(queryStr, function(err) {
+          !err ? resolve() : reject(err);
+        });
+      });
+    },
+    post: function (user) {
+      return new Promise(function(resolve, reject) {
+        var queryStr = `
+          INSERT IGNORE INTO users (name)
+          SELECT '${user.username}'
+        `;
+        db.query(queryStr, function(err) {
+          !err ? resolve() : reject(err);
+        });
+      });
+    }
   }
 };
-
-module.exports.messages.post({
-  username: 'shea',
-  roomname: 'maine',
-  message: 'hi, im from Montana'
-});
